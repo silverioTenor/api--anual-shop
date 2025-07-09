15d77c01cd08fa4b7ef57aea53cf1de7c4783214
Rodrigo Silvério <silverio_tenor@outlook.com>
2025-07-09 20:16:49 -0300

    build(github): fix type IAddress

--- src/usecase/user/update/update-address.user.usecase.ts
+++ src/usecase/user/update/update-address.user.usecase.ts
@@ -1,18 +1,29 @@
 import IUserRepository from '@domain/aggregate/user/repository/repository.interface';
 import UserFactory from '@domain/aggregate/user/factory/user.factory';
+import { IAddress } from '@domain/aggregate/user/interface/address.interface';
 import { InputUpdateUserAddressDTO } from './update.user.dto';
 
 export default class UpdateUserAddressUseCase {
    constructor(private userRepository: IUserRepository) {}
 
    async execute(input: InputUpdateUserAddressDTO) {
-      const foundUser = await this.userRepository.find(input.address.userId);
+      const foundUser = await this.userRepository.find(input.userId);
 
       if (!foundUser) throw new Error('User not found!');
 
       const user = UserFactory.create(foundUser);
-      user.changeAddress(input.address);
 
-      await this.userRepository.saveAddress(input.address);
+      const inputAddress: IAddress = {
+         userId: input.userId,
+         street: input.street,
+         city: input.city,
+         state: input.state,
+         country: input.country,
+         postalCode: input.postalCode,
+      };
+
+      user.changeAddress(inputAddress);
+
+      await this.userRepository.saveAddress(inputAddress);
    }
 }
