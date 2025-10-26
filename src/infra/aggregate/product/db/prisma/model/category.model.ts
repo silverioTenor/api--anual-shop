6dc59c7ea36420ab751cb5683b1e96cbb99a3bf2
Rodrigo Silvério <silverio_tenor@outlook.com>
2025-10-26 20:06:35 -0300

    Revert "feat(Product): add integration test for creation"
    This reverts commit e9cf9da420ad2c1f577d39cb1f5a5e0799dfa7e5.

--- src/infra/aggregate/product/db/prisma/model/category.model.ts
+++
@@ -1,5 +0,0 @@
-import { prisma } from "@infra/@shared/db/prisma/main";
-
-export default class CategoryModel {
-   static db = prisma.categories;
-}
--- src/usecase/product/create/Create.product.integration.spec.ts
+++ src/usecase/product/create/Create.product.integration.spec.ts
@@ -1,47 +1,5 @@
-import ProductRepository from "@infra/aggregate/product/db/prisma/repository/product.repository";
-import UserRepository from "@infra/aggregate/user/db/prisma/repository/user.repository";
-import CreateProductUseCase from "./Create.product.usecase";
-import CreateUserUseCase from "../../user/create/Create.user.usecase";
-import CategoryModel from "../../../infra/aggregate/product/db/prisma/model/category.model";
-
 describe('Integration test for Product', () => {
-   it('should create a product', async () => {
-      const productRepository = new ProductRepository();
-      const userRepository = new UserRepository();
-      const createProductUseCase = new CreateProductUseCase(productRepository);
-
-      const createUserUseCase = new CreateUserUseCase(userRepository);
-
-      const inputCreateUserDTO = {
-         name: 'William Silver',
-         email: 'w.s@gmali.com',
-         document: '12345678910',
-         phone: '21999999999',
-         password: 'abc123',
-         address: undefined,
-      };
-
-      const outputCreateUserDTO = await createUserUseCase.execute(inputCreateUserDTO);
-
-      const category = await CategoryModel.db.create({
-         data: {
-            name: 'Category Test',
-         }
-      });
-
-      const inputCreateProductDTO = {
-         name: "Product Test",
-         description: "Description Test",
-         oldPrice: 100,
-         price: 80,
-         quantity: 10,
-         userId: outputCreateUserDTO.id,
-         categoryId: category.id,
-      };
-
-      const output = await createProductUseCase.execute(inputCreateProductDTO);
-
-      expect(output).toBeDefined();
-      expect(output).toHaveProperty('id');
+   it('should calc value', () => {
+      expect(1 + 1).toBe(2);
    });
 });
